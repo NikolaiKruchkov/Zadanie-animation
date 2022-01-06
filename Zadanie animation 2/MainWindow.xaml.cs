860eaf92fea84e7a5ba3a84f7a93ab5e0e5aa048
NikolaiKruchkov <47074017+NikolaiKruchkov@users.noreply.github.com>
2022-01-06 17:05:59 +0300

    Add files via upload

---
+++ Zadanie animation 2/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Media.Animation;
+
+
+namespace Zadanie_animation_2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            DoubleAnimation doubleAnimation = new DoubleAnimation();
+            doubleAnimation.From = 0;
+            doubleAnimation.To = -635;
+            doubleAnimation.Duration = TimeSpan.FromSeconds(3);
+            doubleAnimation.DecelerationRatio = 1;
+            doubleAnimation.AutoReverse = true;
+            elps.BeginAnimation(Canvas.TopProperty, doubleAnimation);           
+        }
+    }
+}
